d4ebbe1d854c8db992439baa9446f532725859aa
Will Rolke <will.rolke@gmail.com>
2013-01-09 18:46:02 -0500

    Added laser driver package for our LIDAR (LMS100).

---
+++ RCPRG_laser_drivers/LMS1xx/build/CMakeFiles/LMS100.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/LMS100.dir/src/LMS1xx_node.o"
+  "../bin/LMS100.pdb"
+  "../bin/LMS100"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LMS100.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ RCPRG_laser_drivers/LMS1xx/build/CMakeFiles/set_config.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/set_config.dir/src/setConfig.o"
+  "../bin/set_config.pdb"
+  "../bin/set_config"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/set_config.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
