44698cba21fa41aa83f28535091969b064aeb1ac
Will Rolke <will.rolke@gmail.com>
2013-02-10 20:25:29 -0500

    Able to choose how many objects lidar publishes.

--- lidar_object_publisher/src/lidar_object_publisher.cpp
+++ lidar_object_publisher/src/lidar_object_publisher.cpp
@@ -18,6 +18,10 @@
 
 
 ros::Publisher pub;
+ros::Publisher filtered_pub;
+ros::Publisher filtered2_pub;
+
+const int max_objects = 1;
 
 void cloud_callback(const sensor_msgs::PointCloud2ConstPtr& input) {
  // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
@@ -25,33 +29,36 @@ void cloud_callback(const sensor_msgs::PointCloud2ConstPtr& input) {
   std_msgs::Header header = input->header;
   pcl::fromROSMsg (*input, *cloud);
 
-  ROS_WARN("PointCloud before filtering has: %d data points.", cloud->points.size());
+  ROS_DEBUG("PointCloud before filtering has: %d data points.", cloud->points.size());
 
   // Create the filtering object: downsample the dataset using a leaf size of 1cm
   pcl::VoxelGrid<pcl::PointXYZ> vg;
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
   vg.setInputCloud (cloud);
-  vg.setLeafSize (0.01f, 0.01f, 0.01f);
+  vg.setLeafSize (0.01f, 0.01f, 0.01f);//adjusts the voxel size (0.1 = .1 meter voxel) this gives us 1cm voxels
   vg.filter (*cloud_filtered);
-  ROS_WARN("PointCloud after filtering has: %d data points.", cloud_filtered->points.size());
+  ROS_DEBUG("PointCloud after filtering has: %d data points.", cloud_filtered->points.size());
+
+
+  std_msgs::Header filtered_header = cloud_filtered->header;
+  sensor_msgs::PointCloud2 filtered_out;
+  pcl::toROSMsg(*cloud_filtered, filtered_out);
+  filtered_out.header = filtered_header;
+  filtered_pub.publish(filtered_out);
+
 
   // Create the segmentation object for the planar model and set all the parameters
   pcl::SACSegmentation<pcl::PointXYZ> seg;
   pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
   pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZ> ());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_sphere (new pcl::PointCloud<pcl::PointXYZ> ());
   pcl::PCDWriter writer;
   seg.setOptimizeCoefficients (true); //true
   seg.setModelType (pcl::SACMODEL_SPHERE);
   seg.setMethodType (pcl::SAC_RANSAC);
-  seg.setMaxIterations (100);
+  seg.setMaxIterations (10000);
   seg.setDistanceThreshold (0.02);
-
-
-
-  seg.setRadiusLimits(.1, 0.5);
-
-
+  seg.setRadiusLimits(.5, 0.1);// we want our sphere to have RADIUS between x and y METERS
 
   int i=0, nr_points = (int) cloud_filtered->points.size ();
   while (cloud_filtered->points.size () > 0.3 * nr_points)
@@ -61,7 +68,7 @@ void cloud_callback(const sensor_msgs::PointCloud2ConstPtr& input) {
     seg.segment (*inliers, *coefficients);
     if (inliers->indices.size () == 0)
     {
-      ROS_WARN("Could not estimate a planar model for the given dataset.");
+      ROS_WARN("Could not estimate a spherical model for the given dataset.");
       break;
     }
 
@@ -72,8 +79,17 @@ void cloud_callback(const sensor_msgs::PointCloud2ConstPtr& input) {
     extract.setNegative (false);
 
     // Write the planar inliers to disk
-    extract.filter (*cloud_plane);
-    ROS_WARN("PointCloud representing the planar component: %d data points.", cloud_plane->points.size());
+    extract.filter (*cloud_sphere);
+    ROS_DEBUG("PointCloud representing the planar component: %d data points.", cloud_sphere->points.size());
+
+
+    std_msgs::Header filtered2_header = cloud_sphere->header;
+    sensor_msgs::PointCloud2 filtered2_out;
+    pcl::toROSMsg(*cloud_sphere, filtered2_out);
+    filtered2_out.header = filtered2_header;
+    filtered2_pub.publish(filtered2_out);
+
+
 
     // Remove the planar inliers, extract the rest
     extract.setNegative (true);
@@ -95,16 +111,16 @@ void cloud_callback(const sensor_msgs::PointCloud2ConstPtr& input) {
   ec.extract (cluster_indices);
 
   int j = 0;
-  for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+  for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); (it != cluster_indices.end()) && (j < max_objects); ++it)
   {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
-    for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); pit++)
-      cloud_cluster->points.push_back (cloud_filtered->points[*pit]);
-    cloud_cluster->width = cloud_cluster->points.size ();
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
+    for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); pit++)
+      cloud_cluster->points.push_back(cloud_filtered->points[*pit]);
+    cloud_cluster->width = cloud_cluster->points.size();
     cloud_cluster->height = 1;
     cloud_cluster->is_dense = true;
 
-    ROS_WARN("PointCloud representing the Cluster: %d data points.", cloud_cluster->points.size());
+    ROS_DEBUG("PointCloud representing the Cluster: %d data points.", cloud_cluster->points.size());
 
     sensor_msgs::PointCloud2 cloud_out;
     pcl::toROSMsg(*cloud_cluster, cloud_out);
@@ -115,6 +131,28 @@ void cloud_callback(const sensor_msgs::PointCloud2ConstPtr& input) {
     j++;
   }
 
+/*
+  int j = 0;
+  for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+  {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+    for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); pit++)
+      cloud_cluster->points.push_back (cloud_filtered->points[*pit]);
+    cloud_cluster->width = cloud_cluster->points.size ();
+    cloud_cluster->height = 1;
+    cloud_cluster->is_dense = true;
+
+    ROS_DEBUG("PointCloud representing the Cluster: %d data points.", cloud_cluster->points.size());
+
+    sensor_msgs::PointCloud2 cloud_out;
+    pcl::toROSMsg(*cloud_cluster, cloud_out);
+    cloud_out.header = header;
+    pub.publish(cloud_out);
+    ROS_ERROR("PUBLISHED!!");
+
+    j++;
+  }
+*/
 }
 
 int main(int argc, char** argv) {
@@ -126,7 +164,9 @@ int main(int argc, char** argv) {
   ros::Subscriber sub = nh.subscribe ("/cloud", 1, cloud_callback);
 
   // Create a ROS publisher for the output point cloud
-  pub = nh.advertise<sensor_msgs::PointCloud2> ("/filtered_cloud", 1);
+  pub = nh.advertise<sensor_msgs::PointCloud2> ("/lidar_object", 1);
+  filtered_pub = nh.advertise<sensor_msgs::PointCloud2> ("/filtered_cloud1", 1);
+  filtered2_pub = nh.advertise<sensor_msgs::PointCloud2> ("/filtered_cloud2", 1);
 
   // Spin
   ros::spin ();
