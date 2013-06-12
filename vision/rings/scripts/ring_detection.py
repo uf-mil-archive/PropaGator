6697cf83113b7493baca0aa81551af0f9d841489
Navid Shahrestani <nshahrestani@mil-wall-e-x.mil.ece.ufl.edu>
2013-06-12 14:42:15 -0400

    rins:cleaned code up

--- vision/rings/scripts/ring_detection.py
+++ vision/rings/scripts/ring_detection.py
@@ -10,13 +10,14 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from visualization_msgs.msg import Marker,MarkerArray
 import actionlib
-from c3_trajectory_generator.msg import MoveToAction, MoveToGoal
-from uf_common.orientation_helpers import lookat, get_perpendicular
+from geometry_msgs.msg import Point
 from nav_msgs.msg import Odometry
 from rings.msg import ShootRingsAction,ShootRingsActionFeedback
 
 rospy.init_node('ring_detection')
 bridge = CvBridge()
+
+carrot = rospy.Publisher("/carrot",Point)
 
 global running,current_position,shots,orientation,colors,color_index
 running = False
@@ -28,36 +29,9 @@ color_index = 0
 
 #-----------------------------------------------------------------------------------
 
-waypoint = actionlib.SimpleActionClient('moveto', MoveToAction)
-print 'connecting to action client'
-#waypoint.wait_for_server()
-
-def send_waypoint(point,orientation):
-	waypoint.send_goal(MoveToGoal(
-			header=Header(
-				frame_id='/simmap',
-			),
-			pose=Pose(
-				position=Point(
-					x=point[0],
-					y=point[1],
-					z=0,
-				),
-				orientation=Quaternion(*orientation),
-			),
-	))
-
-def distance (p1,p2):
-	return (math.sqrt((p2[1]-p1[1])**2 + (p2[0]-p1[0])**2))
-#-----------------------------------------------------------------------------------
-
-#parameters to set
-RED_MIN = cv.fromarray(numpy.array([0, 125, 0],numpy.uint8),allowND = True)
-RED_MAX = cv.fromarray(numpy.array([15, 255, 60],numpy.uint8),allowND = True)
-
 purple_MIN = cv.fromarray(numpy.array([30, 160, 50],numpy.uint8),allowND = True)
 purple_MAX = cv.fromarray(numpy.array([75, 210, 130],numpy.uint8),allowND = True)
-OBJECT_AREA = 10000
+OBJECT_AREA = 15000
 IMAGE_SIZE = (640,480)
 
 #-----------------------------------------------------------------------------------
@@ -69,10 +43,8 @@ blurred_image = cv.CreateImage(IMAGE_SIZE,8,3)
 h_channel = cv.CreateImage(IMAGE_SIZE,8,1)
 s_channel = cv.CreateImage(IMAGE_SIZE,8,1)  
 v_channel = cv.CreateImage(IMAGE_SIZE,8,1)
+h_not = cv.CreateImage(IMAGE_SIZE,8,1)
 blurred_bgr_image = cv.CreateImage(IMAGE_SIZE,8,3)
-
-red_threshold_image = cv.CreateImage(IMAGE_SIZE,8,1) 
-purple_threshold_image = cv.CreateImage(IMAGE_SIZE,8,1)
 
 red_adaptive = cv.CreateImage(IMAGE_SIZE,8,1)
 purple_adaptive = cv.CreateImage(IMAGE_SIZE,8,1)
@@ -80,7 +52,6 @@ red_eroded_image = cv.CreateMat(IMAGE_SIZE[1],IMAGE_SIZE[0],cv.CV_8U)
 red_dilated_image = cv.CreateMat(IMAGE_SIZE[1],IMAGE_SIZE[0],cv.CV_8U)
 purple_eroded_image = cv.CreateMat(IMAGE_SIZE[1],IMAGE_SIZE[0],cv.CV_8U)
 purple_dilated_image = cv.CreateMat(IMAGE_SIZE[1],IMAGE_SIZE[0],cv.CV_8U)
-
 
 #-----------------------------------------------------------------------------------
 
@@ -91,19 +62,23 @@ def mouse_callback(event,x,y,flags,image):
                 print cv.Get2D(image,y,x)                                     
 
 #-----------------------------------------------------------------------------------
+gain = .1
+red_shoot = (320,240)
+def distance (p1,p2):
+	return (math.sqrt((p2[1]-p1[1])**2 + (p2[0]-p1[0])**2))
 def adjust_carrot(x,y):
-        err = distance((x,y),current_position)
-        offset = err*.5
-        if (offset > .1):
-                send_waypoint((offset + current_position[0],offset - current_position[1]),orientation)
+        err = distance((x,y),red_shoot)
+        if (err > 10):
+                carrot.publish(Point(x = gain*(x - red_shoot[0]),y = gain*(y - red_shoot[1]),z = 0))
         else:
                 print "shooting!"
                 color_index = color_index + 1
                 shots = shots + 1      
 
 def image_callback(data):
+        global running,colors
         if (running):
-                print "running"
+                #print "running"
                 cv_image = bridge.imgmsg_to_cv(data,"bgr8")
                 cv.CvtColor(cv_image,hsv_image,cv.CV_BGR2HSV)                         # --convert from BGR to HSV
 
@@ -113,16 +88,17 @@ def image_callback(data):
                
                 cv.Split(hsv_image,h_channel,s_channel,v_channel,None)                #split HSV image into three seperate images
                 
-                
-                cv.AdaptiveThreshold(s_channel,red_adaptive,255,cv.CV_ADAPTIVE_THRESH_MEAN_C,cv.CV_THRESH_BINARY,33,-26)      #use hue channel to filter for red
+                #cv.Not(h_channel,h_not)
+                cv.AdaptiveThreshold(s_channel,red_adaptive,255,cv.CV_ADAPTIVE_THRESH_MEAN_C,cv.CV_THRESH_BINARY,53,-20)      #use hue channel to filter for red
                 cv.AdaptiveThreshold(s_channel,purple_adaptive,255,cv.CV_ADAPTIVE_THRESH_MEAN_C,cv.CV_THRESH_BINARY_INV,53,26)
+                cv.ShowImage("red threshold",red_adaptive)
                
-                cv.Erode(red_adaptive,red_eroded_image,None,1)                        #erode and dilate the thresholded images
-                cv.Erode(purple_threshold_image,purple_eroded_image,None,15)
+                cv.Erode(red_adaptive,red_eroded_image,None,3)                        #erode and dilate the thresholded images
+                cv.Erode(purple_adaptive,purple_eroded_image,None,15)
                 cv.Dilate(red_eroded_image,red_dilated_image,None,10)
                 cv.Dilate(purple_eroded_image,purple_dilated_image,None,9)
 
-                #cv.ShowImage("red threshold",red_dilated_image)                       #show image here because findContours effects memory location
+                cv.ShowImage("red threshold",red_dilated_image)                       #show image here because findContours effects memory location
                 
                 red_contours,_ = cv2.findContours(image=numpy.asarray(red_dilated_image[:,:]),mode=cv.CV_RETR_EXTERNAL,method=cv.CV_CHAIN_APPROX_SIMPLE)
                 purple_contours,_ = cv2.findContours(image=numpy.asarray(purple_dilated_image[:,:]),mode=cv.CV_RETR_EXTERNAL,method=cv.CV_CHAIN_APPROX_SIMPLE)
@@ -138,7 +114,7 @@ def image_callback(data):
                                         y = int(cv.GetSpatialMoment(moments, 0, 1)/area)
                                         radius = int(math.sqrt(area/math.pi))
                                         cv.Circle(cv_image,(x,y),radius,(0,0,255),3)
-                                        if (color[color_index] == 'red'):
+                                        if (colors[color_index] == 'red'):
                                                 adjust_carrot(x,y)
                                         
                 if (purple_contours):
@@ -150,17 +126,11 @@ def image_callback(data):
                                         y = int(cv.GetSpatialMoment(moments, 0, 1)/area)
                                         radius = int(math.sqrt(area/math.pi))
                                         cv.Circle(cv_image,(x,y),radius,(0,255,0),3)
-                                        if (color[color_index] == 'purple'):
+                                        if (colors[color_index] == 'purple'):
                                                 purple_ring_pos = (x,y)
 
                               
-                                
-                 
-
-
-                         
                 cv.SetMouseCallback("camera feed",mouse_callback,hsv_image)   
-                #cv.ShowImage("test",dist_image)             
                 #cv.ShowImage("H channel",h_channel)
                 #cv.ShowImage("S channel",s_channel)
                 #cv.ShowImage("V channel",v_channel)
@@ -170,24 +140,17 @@ def image_callback(data):
 
 #-----------------------------------------------------------------------------------
 
-def pose_callback(msg):
-	global current_position,orientation
-        orientation = msg.pose.pose.orientation
-	current_position = (msg.pose.pose.position.x,msg.pose.pose.position.y)
-        
-rospy.Subscriber('/odom', Odometry, pose_callback)
-#-----------------------------------------------------------------------------------
-
 class ShootRingsServer:
 
  def __init__(self):
+        global running
         self.server = actionlib.SimpleActionServer('shoot_rings', ShootRingsAction, self.execute, False)
         rospy.Subscriber("/mv_bluefox_camera_node/image_raw",Image,image_callback)
         self.server.start()
         self._feedback = ShootRingsActionFeedback
 
  def execute(self,goal):
-        global shots
+        global shots,running
         while(shots < goal.attempts):
                 running = True
                 self._feedback.darts_shot = shots
