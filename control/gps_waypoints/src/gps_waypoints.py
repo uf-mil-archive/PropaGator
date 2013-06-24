4eb492422b5a67806c1867c47cd40d479b4ca280
navid <navid@navid-VPCSA290X.(none)>
2013-06-23 20:11:03 -0400

    added new /gps2_parser/pos topic

--- control/gps_waypoints/src/gps_waypoints.py
+++ control/gps_waypoints/src/gps_waypoints.py
@@ -4,10 +4,11 @@ import roslib
 import rospy
 roslib.load_manifest('gps_waypoints')
 from uf_common.msg import MoveToAction, MoveToGoal
+from geometry_msgs.msg import PointStamped,Point
 from sensor_msgs.msg import NavSatFix
 from nav_msgs.msg import Odometry
 from rawgps_common.gps import ecef_from_latlongheight,enu_from_ecef
-import threading
+import threading,numpy
 import actionlib
 
 rospy.init_node('gps_waypoints')
@@ -15,29 +16,43 @@ rospy.init_node('gps_waypoints')
 
 waypoint = actionlib.SimpleActionClient('moveto', MoveToAction)
 print 'connecting to action client'
-waypoint.wait_for_server()
+#waypoint.wait_for_server()
 
 global pos,origin
 pos = [0,0,0]
-origin = []
+origin = [0,0,0]
 
-def waypoint_callback(msg):
+def waypoint_latlong_callback(msg):
         global pos,origin
 
         ecef = ecef_from_latlongheight(msg.latitude, msg.longitude, msg.altitude)
-        diff = ecef - pos
+        diff = numpy.array(ecef) - numpy.array(pos)
 
         goal = enu_from_ecef(diff,origin)
-        waypoint.send_goal_and_wait('world',goal)
+        print goal
+        #waypoint.send_goal_and_wait('world',(goal[0],goal[1],0))
+
+def waypoint_ecef_callback(msg):
+        global pos,origin
+
+        ecef = [msg.point.x,msg.point.y,msg.point.z]
+        diff = numpy.array(ecef) - numpy.array(pos)
+        print 'diff',diff
+        goal = enu_from_ecef(diff,origin)
+        print 'ecef',ecef
+        print 'pos',pos
+        print 'goal',goal
+        #waypoint.send_goal_and_wait('world',(goal[0],goal[1],0))
 
 
 def pos_callback(msg):
         global pos,origin
 
-        if (not origin):
-                origin = [msg.x,msg.y,msg.z]
-        pos = [msg.x,msg.y,msg.z]
+        if (origin == [0,0,0]):
+                origin = [msg.point.x,msg.point.y,msg.point.z]
+        pos = [msg.point.x,msg.point.y,msg.point.z]
 
-rospy.Subscriber('/gps_latlong_waypoint',NavSatFix,waypoint_callback)
-rospy.Subscriber('/gps_pos',NavSatFix,pos_callback)
+rospy.Subscriber('/gps_latlong_waypoint',NavSatFix,waypoint_latlong_callback)
+rospy.Subscriber('/gps_ecef_waypoint',PointStamped,waypoint_ecef_callback)
+rospy.Subscriber('/gps2_parser/pos',PointStamped,pos_callback)
 rospy.spin()
