3a4a4abeb210d4639095e687973e1bc2a039ad0f
Navid Shahrestani <nshahrestani@mil-wall-e-x.mil.ece.ufl.edu>
2013-06-24 23:12:01 -0400

    rings:still messing with threholds

--- vision/rings/scripts/ring_detection.py
+++ vision/rings/scripts/ring_detection.py
@@ -33,25 +33,56 @@ color_index = 0
 
 purple_MIN = cv.fromarray(numpy.array([30, 160, 50],numpy.uint8),allowND = True)
 purple_MAX = cv.fromarray(numpy.array([75, 210, 130],numpy.uint8),allowND = True)
-OBJECT_AREA = 15000
+OBJECT_AREA = 1500
 IMAGE_SIZE = (640,480)
 
 #-----------------------------------------------------------------------------------
-hsv_image = cv.CreateImage(IMAGE_SIZE,8,3)
-lab_image = cv.CreateImage(IMAGE_SIZE,8,3)
-blurred_image = cv.CreateImage(IMAGE_SIZE,8,3)
-h_channel = cv.CreateImage(IMAGE_SIZE,8,1)
-s_channel = cv.CreateImage(IMAGE_SIZE,8,1)  
-v_channel = cv.CreateImage(IMAGE_SIZE,8,1)
-l_channel = cv.CreateImage(IMAGE_SIZE,8,1)
-a_channel = cv.CreateImage(IMAGE_SIZE,8,1)  
-b_channel = cv.CreateImage(IMAGE_SIZE,8,1)
-r = cv.CreateImage(IMAGE_SIZE,8,1)
-g = cv.CreateImage(IMAGE_SIZE,8,1)  
-b = cv.CreateImage(IMAGE_SIZE,8,1)
+blurred_gray = cv.CreateImage(IMAGE_SIZE,8,1)
+gray = cv.CreateImage(IMAGE_SIZE,8,1)
+
+hsv = cv.CreateImage(IMAGE_SIZE,8,3)
+hsv_h = cv.CreateImage(IMAGE_SIZE,8,1)
+hsv_s = cv.CreateImage(IMAGE_SIZE,8,1)  
+hsv_v = cv.CreateImage(IMAGE_SIZE,8,1)
+
+lab = cv.CreateImage(IMAGE_SIZE,8,3)
+lab_l = cv.CreateImage(IMAGE_SIZE,8,1)
+lab_a = cv.CreateImage(IMAGE_SIZE,8,1)  
+lab_b = cv.CreateImage(IMAGE_SIZE,8,1)
+
+rgb_r = cv.CreateImage(IMAGE_SIZE,8,1)
+rgb_g = cv.CreateImage(IMAGE_SIZE,8,1)  
+rgb_b = cv.CreateImage(IMAGE_SIZE,8,1)
+
+luv = cv.CreateImage(IMAGE_SIZE,8,3)
+luv_l = cv.CreateImage(IMAGE_SIZE,8,1)
+luv_u = cv.CreateImage(IMAGE_SIZE,8,1)  
+luv_v = cv.CreateImage(IMAGE_SIZE,8,1)
+
+xyz = cv.CreateImage(IMAGE_SIZE,8,3)
+xyz_x = cv.CreateImage(IMAGE_SIZE,8,1)
+xyz_y = cv.CreateImage(IMAGE_SIZE,8,1)  
+xyz_z = cv.CreateImage(IMAGE_SIZE,8,1)
+
+hls = cv.CreateImage(IMAGE_SIZE,8,3)
+hls_h = cv.CreateImage(IMAGE_SIZE,8,1)
+hls_l = cv.CreateImage(IMAGE_SIZE,8,1)  
+hls_s = cv.CreateImage(IMAGE_SIZE,8,1)
+
+xyz = cv.CreateImage(IMAGE_SIZE,8,3)
+xyz_x = cv.CreateImage(IMAGE_SIZE,8,1)
+xyz_y = cv.CreateImage(IMAGE_SIZE,8,1)  
+xyz_z = cv.CreateImage(IMAGE_SIZE,8,1)
+
+ycrcb = cv.CreateImage(IMAGE_SIZE,8,3)
+ycrcb_y = cv.CreateImage(IMAGE_SIZE,8,1)
+ycrcb_cr = cv.CreateImage(IMAGE_SIZE,8,1)  
+ycrcb_cb = cv.CreateImage(IMAGE_SIZE,8,1)
 
 sa = cv.CreateImage(IMAGE_SIZE,8,1)
 a_not = cv.CreateImage(IMAGE_SIZE,8,1)
+test = cv.CreateImage(IMAGE_SIZE,8,1)
+final = cv.CreateImage(IMAGE_SIZE,8,1)
 
 red_adaptive = cv.CreateImage(IMAGE_SIZE,8,1)
 purple_adaptive = cv.CreateImage(IMAGE_SIZE,8,1)
@@ -77,7 +108,8 @@ def adjust_carrot(x,y):
         err = distance((x,y),red_shoot)
         adjust = (x-red_shoot[0],y-red_shoot[1])
         if (err > 10):
-                print "move by: ",adjust
+                pass
+                #print "move by: ",adjust
                 #waypoint.send_goal(current_pose_editor.relative(numpy.array([x, y, 0])).as_MoveToGoal(speed = .1))
         else:
                 print "shooting!"
@@ -91,8 +123,8 @@ def threshold_purple(image):
         cv.Dilate(purple_adaptive,purple_dilated_image,None,8)
 
 def threshold_red(image):
-        cv.AdaptiveThreshold(image,red_adaptive,255,cv.CV_ADAPTIVE_THRESH_MEAN_C,cv.CV_THRESH_BINARY,57,-7)
-        cv.Erode(red_adaptive,red_eroded_image,None,2)
+        cv.AdaptiveThreshold(image,red_adaptive,255,cv.CV_ADAPTIVE_THRESH_MEAN_C,cv.CV_THRESH_BINARY,57,-10)
+        cv.Erode(red_adaptive,red_eroded_image,None,1)
         cv.Dilate(red_eroded_image,red_dilated_image,None,5)    
 
 #-----------------------------------------------------------------------------------   
@@ -107,9 +139,10 @@ def extract_circles(contours,rgb):
                         x = int(cv.GetSpatialMoment(moments, 1, 0)/area)
                         y = int(cv.GetSpatialMoment(moments, 0, 1)/area)
                         radius = int(math.sqrt(area/math.pi))
-                        circles.append((x,y,int(radius)))
-                        if (colors[color_index] == 'red'):
-                                adjust_carrot(x,y)          
+                        if (y < 200):
+                                circles.append((x,y,int(radius)))
+                                if (colors[color_index] == 'red'):
+                                        adjust_carrot(x,y)          
         return circles 
 
 #-----------------------------------------------------------------------------------    
@@ -119,46 +152,92 @@ def image_callback(data):
         if (running):
                 #print "running"
                 cv_image = bridge.imgmsg_to_cv(data,"bgr8")
-                cv.CvtColor(cv_image,hsv_image,cv.CV_BGR2HSV)
-                cv.CvtColor(cv_image,lab_image,cv.CV_BGR2Lab)                        
 
-                cv.Smooth(hsv_image,blurred_image,cv.CV_GAUSSIAN,5,5)                
-                cv.Split(blurred_image,h_channel,s_channel,v_channel,None)
-                cv.Split(cv_image,r,g,b,None)
-                cv.Split(lab_image,l_channel,a_channel,b_channel,None)
+                cv.CvtColor(cv_image,hsv,cv.CV_BGR2HSV)
 
-                cv.Not(a_channel,a_not)
-                cv.Sub(s_channel,a_not,sa)
+                cv.CvtColor(cv_image,lab,cv.CV_BGR2Lab)
+                cv.CvtColor(cv_image,luv,cv.CV_BGR2Luv)
+                cv.CvtColor(cv_image,hls,cv.CV_BGR2HLS)
+                cv.CvtColor(cv_image,xyz,cv.CV_BGR2XYZ)
+                cv.CvtColor(cv_image,ycrcb,cv.CV_BGR2YCrCb)                       
 
+               
+                cv.Split(hsv,hsv_h,hsv_s,hsv_v,None)
+                cv.Split(cv_image,rgb_r,rgb_g,rgb_b,None)
+                cv.Split(lab,lab_l,lab_a,lab_b,None)
+                cv.Split(luv,luv_l,luv_u,luv_v,None)
+                cv.Split(hls,hls_h,hls_l,hls_s,None)
+                cv.Split(xyz,xyz_x,xyz_y,xyz_x,None)
+                cv.Split(ycrcb,ycrcb_y,ycrcb_cr,ycrcb_cb,None)
+
+                cv.Not(lab_a,a_not)
+                cv.Sub(hsv_s,a_not,sa)
+                #cv.Sub(luv_u,hls_h,test)
+                cv.Sub(luv_u,hls_h,test)
+
+                '''
+                cv.CvtColor(cv_image,gray,cv.CV_BGR2GRAY) 
+                cv.Smooth(gray,blurred_gray,cv.CV_GAUSSIAN,3,3) 
+                small = cv2.resize(numpy.asarray(ycrcb_cr[:,:]),(320,240)
+                circles=cv2.HoughCircles(image=numpy.asarray(small[:,:]),method=cv.CV_HOUGH_GRADIENT,dp=1,minDist=1,param1=100,param2=60, minRadius=1,maxRadius=600)
+
+                if not(circles is None):
+                        for i in circles:
+                                if (i[0][1] < 200):
+                                        print "found circles",i,len(i)
+                                        center = (i[0][0],i[0][1])
+                                        radius = i[0][2]
+                                        cv.Circle(cv_image,center,radius,(1,1,0),2) 
+                '''
                 
+              
+                threshold_purple(hsv_s)
+                threshold_red(ycrcb_cr)  
+                cv.Mul(test,red_adaptive,final)
 
-                threshold_purple(s_channel)
-                threshold_red(sa)  
-                cv.ShowImage("red",red_eroded_image)             
+                cv.ShowImage("red",red_dilated_image)            
+               
 
                 red_contours,_ = cv2.findContours(image=numpy.asarray(red_dilated_image[:,:]),mode=cv.CV_RETR_EXTERNAL,method=cv.CV_CHAIN_APPROX_SIMPLE)
                 purple_contours,_ = cv2.findContours(image=numpy.asarray(purple_dilated_image[:,:]),mode=cv.CV_RETR_EXTERNAL,method=cv.CV_CHAIN_APPROX_SIMPLE)
                 #cv2.drawContours(numpy.asarray(cv_image[:,:]),red_contours,-1,(0,0,255),3)   
+                
+                (_,center,radius) = cv.MinEnclosingCircle(red_contours)
+                cv.Circle(cv_image,center,radius,(1,0,0),3) 
 
                 for i in [ (red_contours,[1,0,0]) , (purple_contours,[1,0,1]) ]:
                         circles = extract_circles(i[0],i[1])
                         rgb = i[1]
                         bgr = (255*numpy.array(rgb[::-1])).tolist()        #invert list and multiply by 255 for cv.Circle color argument
-                        for x,y,radius in circles:                    
-                                cv.Circle(cv_image,(x,y),radius,bgr,3)  
+                        for x,y,radius in circles:  
+                                pass                  
+                                #cv.Circle(cv_image,(x,y),radius,bgr,3)  
 
 
-                cv.SetMouseCallback("camera feed",mouse_callback,hsv_image)   
+                cv.SetMouseCallback("camera feed",mouse_callback,hsv)   
                 '''
-                cv.ShowImage("H channel",h_channel)
-                cv.ShowImage("S channel",s_channel)
-                cv.ShowImage("V channel",v_channel)
-                cv.ShowImage("L channel",l_channel)
-                cv.ShowImage("A channel",a_channel)
-                cv.ShowImage("B channel",b_channel)
-                cv.ShowImage("R",r)
-                cv.ShowImage("G",g)
-                cv.ShowImage("B",b)
+                cv.ShowImage("TEST",test)
+                cv.ShowImage("HSV_H",hsv_h)
+                cv.ShowImage("HSV_S",hsv_s)
+                cv.ShowImage("HSV_V",hsv_v)
+                cv.ShowImage("LAB_L",lab_l)
+                cv.ShowImage("LAB_A",lab_a)
+                cv.ShowImage("LAB_B",lab_b)
+                cv.ShowImage("RGB_R",rgb_r)
+                cv.ShowImage("RGB_G",rgb_g)
+                cv.ShowImage("RGB_B",rgb_b)
+                cv.ShowImage("LUV_L",luv_l)
+                cv.ShowImage("LUV_U",luv_u)
+                cv.ShowImage("LUV_V",luv_v)
+                cv.ShowImage("HLS_H",hls_h)
+                cv.ShowImage("HLS_L",hls_l)
+                cv.ShowImage("HLS_S",hls_s)
+                cv.ShowImage("XYZ_X",xyz_x)
+                cv.ShowImage("XYZ_Y",xyz_y)
+                cv.ShowImage("XYZ_Z",xyz_z)
+                cv.ShowImage("YCrCb_Y",ycrcb_y)
+                cv.ShowImage("YCrCb_Cr",ycrcb_cr)
+                cv.ShowImage("YCrCb_Cb",ycrcb_cb)
                 '''
                 cv.ShowImage("camera feed",cv_image)
                
