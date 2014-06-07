f6fd5e12668c9bb56c5b7381b2352bd86cc96bfb
Kevin French <fnivek@gmail.com>
2014-06-07 06:52:55 -0400

    Added Newton --> PWM conversion for boat thrusters

---
+++ motor_control/src/pwm_for_motor.ino
@@ -0,0 +1,51 @@
+//Victor 888 
+
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+
+int servoID,thrust;
+
+void setup() {
+ servo3.attach(9);
+ servo2.attach(6);
+ Serial.begin(9600);
+ Serial.println("Enter servo# (2 or 3) and thrust (45-145): ");
+}
+
+void loop() {
+  
+  if(Serial.available() > 0){
+    
+    servoID = Serial.parseInt();
+    thrust = Serial.parseInt();
+    Serial.read();
+    
+    Serial.print("Servo ID: ");
+    Serial.println(servoID);
+    
+    Serial.print("thrust: ");
+    Serial.println(thrust);
+    
+   if(servoID == 2){
+     servo2.write(thrust);
+     delay(15);
+   }
+   else if (servoID == 3){
+     servo3.write(thrust);
+     delay(15);
+   }
+   else {
+     Serial.println("Invalid servo ID");
+   }
+   
+   
+
+   }
+   
+   
+   
+    
+  }
+
