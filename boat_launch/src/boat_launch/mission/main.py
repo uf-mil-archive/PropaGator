fac8fd9daa9bf0e17d693ca3554f7f44af2c19fb
Forrest Voight <forrest@forre.st>
2014-07-11 10:03:48 -0400

    boat_launch: mission changes

--- boat_launch/src/boat_launch/mission/main.py
+++ boat_launch/src/boat_launch/mission/main.py
@@ -9,18 +9,27 @@ import boat_scripting
 
 from boat_launch.mission import dock2, acoustic_beacon
 
+def ll(lat, lon):
+    return gps.ecef_from_latlongheight([math.radians(lat), math.radians(lon), 0])
+
 
 @util.cancellableInlineCallbacks
 def main_list(nh, boat, course):
     try:
         print 'main start'
+        if course in ['A', 'B']:
+            yield boat.move.heading_deg(90-298).go()
+            yield boat.move.forward(30).go()
         yield boat.go_to_ecef_pos(dict(
             pool=[1220416.51743, -4965356.4575, 3799838.03177],
+            A=ll(36.802101, -76.191527), # from google earth
+            B=ll(36.802101, -76.191527), # from google earth
         )[course])
         print "starting dock2"
         yield boat.go_to_ecef_pos(dict(
             pool=[1220427.17101, -4965353.32773, 3799835.96511],
-            A=[1220411.29049, -4965401.85057, 3799840.98773],
+            A=ll(36.80225, -76.19128),
+            B=ll(36.80174, -76.19189),
         )[course])
         yield boat.move.heading_deg(dict(
             pool=60,
