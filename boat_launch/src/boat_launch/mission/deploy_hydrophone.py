b6503b5a59c4b676bb1277dd40e5a8ecbe1a32b9
Kevin French <fnivek@gmail.com>
2014-12-14 21:23:16 -0500

    Removed the ability to rosrun these misions

--- boat_launch/src/boat_launch/mission/deploy_hydrophone.py
+++ boat_launch/src/boat_launch/mission/deploy_hydrophone.py
@@ -2,36 +2,13 @@
 
 from __future__ import division
 
-import signal
-
-import txros
 from txros import util
-
-from twisted.internet import defer, reactor
 
 import boat_scripting
 
 
 @util.cancellableInlineCallbacks
 def main(nh):
-    boat = yield boat_scripting.get_boat(nh)
+    boat = yield boat_scripting.get_boat(nh, False)
     
     yield boat.deploy_hydrophone()
-
-@util.cancellableInlineCallbacks
-def _main():
-    try:
-    	nh = txros.NodeHandle.from_argv('deploy_hydrophone', anonymous=True)
-        yield main(nh)
-    except Exception:
-        traceback.print_exc()
-    finally:
-        reactor.stop()
-
-def _start():
-    signal.signal(signal.SIGINT, lambda signum, frame: reactor.callFromThread(task.cancel))
-    task = _main().addErrback(lambda fail: fail.trap(defer.CancelledError))
-
-if __name__ == '__main__':
-    reactor.callWhenRunning(_start)
-    reactor.run()
