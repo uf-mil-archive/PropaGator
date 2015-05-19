e79bf7612413eb3f64441d2a27cbf53aa71330b5
Kevin French <fnivek@gmail.com>
2015-05-19 11:53:26 -0400

    Integrated kill handling

--- remote_control/src/rc_script.py
+++ remote_control/src/rc_script.py
@@ -13,6 +13,7 @@
 ### Authored by Khimya Khetarpal on March,20,2015
 
 ### Please update revisons to it with your name and date and valuable comments. 
+###	Github handles this just look at the file history or use git blame (Kevin French)
 
 import rospy
 
@@ -21,7 +22,12 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Header
 from std_msgs.msg import Float64
-killall = 0
+
+from kill_handling.listener import KillListener
+from kill_handling.broadcaster import KillBroadcaster
+
+killed = False
+zero_pwm = 1.5e-3
 
 X360_AXIS_IDS = {
 	'LEFT_STICK_X': 0,
@@ -46,36 +52,73 @@ X360_BUTTONS_IDS = {
 pwm_port_pub = rospy.Publisher('stm32f3discovery_imu_driver/pwm1', Float64, queue_size = 10)
 pwm_starboard_pub = rospy.Publisher('stm32f3discovery_imu_driver/pwm2', Float64, queue_size = 10)
 
+def killed_cb():
+	global killed
+	killed = True
+	zero_pwms()
+	rospy.logwarn('%s killed because %s' % (rospy.get_name(), kill_listener.get_kills()) )
+
+def unkilled_cb():
+	global killed
+	killed = False
+	rospy.loginfo('%s unkilled' % rospy.get_name())
+
+def on_shutdown():
+	# Kill system on shutdown
+	kill_broadcaster.send(True)
+
+def zero_pwms():
+	pwm1 = Float64(zero_pwm)
+	pwm_port_pub.publish(pwm1)
+	pwm2 = Float64(zero_pwm)
+	pwm_starboard_pub.publish(pwm2)
 
 def start():
+	global kill_listener
+	global kill_broadcaster
+
 	#Init node
-	rospy.init_node('remote_control', anonymous=True)
-	
+	rospy.init_node('remote_control')
+
+	kill_broadcaster = KillBroadcaster(id=rospy.get_name(), description='Remote control kill')
+	kill_listener = KillListener(killed_cb, unkilled_cb)
+	try:
+		kill_broadcaster.clear()
+	except rospy.service.ServiceException, e:
+		rospy.logwarn(str(e))
 	
 	#Init subscribers and publishers
 	rospy.Subscriber('joy', Joy, xbox_cb, queue_size = 10)
+
+	# Add a shutdown hook
+	rospy.on_shutdown(on_shutdown)
 
 	#Spin
 	rospy.spin()
 
 def xbox_cb(joy_msg):
-	global killall
+	global killed
 	
+	# If kill_broadcaster.send(killed) is put outside these if statments then
+	#	rc will echo any other kill with its own kill msg which prevents the RC node from ever coming out of a kill
+	#	without pressing the start button
 	if (joy_msg.buttons[8] == 1):	#Press KILL Switch
-		killall = 1
-	if (joy_msg.buttons[7] == 1):	#Press START Switch
-		killall = 0
+		# Kill the boat
+		killed = True
+		kill_broadcaster.send(True)
 
+	if (joy_msg.buttons[7] == 1):	#Press START Switch
+		# Do not update kill here as there may be other kills that affect the weather the boat should or shouldn't be killed
+		kill_broadcaster.send(False)
+
+	
 		
-	if (killall == 1):
-		pwm1 = Float64(0.0015)
-		pwm_port_pub.publish(pwm1)
-		pwm2 = Float64(0.0015)
-		pwm_starboard_pub.publish(pwm2)
+	if killed:
+		zero_pwms()
 	else:
-		pwm1 = Float64(0.0005*(joy_msg.axes[1]) + 0.0015)  #LEFT_STICK
+		pwm1 = Float64(0.0005*(joy_msg.axes[1]) + zero_pwm)  #LEFT_STICK
 		pwm_port_pub.publish(pwm1)
-		pwm2 = Float64(0.0005*(joy_msg.axes[3]) + 0.0015)  #RIGHT_STICK
+		pwm2 = Float64(0.0005*(joy_msg.axes[3]) + zero_pwm)  #RIGHT_STICK
 		pwm_starboard_pub.publish(pwm2) 
 
 	
