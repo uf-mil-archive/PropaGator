f5f31b15086e8ce326b78b7f8939d61a2ff9e813
Zach Goins <zach.a.goins@gmail.com>
2015-05-19 01:06:15 -0400

    Azi_drive: Add kill node and bash support

--- azi_drive/src/azimuth_drive/azi_waypoint.py
+++ azi_drive/src/azimuth_drive/azi_waypoint.py
@@ -6,38 +6,63 @@ import rospy
 # Brings in the SimpleActionaction
 import actionlib
 from uf_common.msg import PoseTwistStamped, MoveToAction
+from kill_handling.listener import KillListener
+
+'''
+
+TODO: Right now values are oonly coming in from the waypoint node
+      Still need to send those values to azi_drive
+
+'''
 
 class send_action:
 
     def __init__(self):
-        self.client = actionlib.SimpleActionServer('moveto', MoveToAction, self.new_goal, False)
+        self.action = actionlib.SimpleActionServer('moveto', MoveToAction, self.new_goal, False)
         self.trajectory_pub = rospy.Publisher('/trajectory', PoseTwistStamped, queue_size=10)
-        self.client.start()
-        self.to_goal_posetwist = PoseTwistStamped()
-        self.to_goal_header = PoseTwistStamped()
+        self.kill_listener = KillListener(self.set_kill, self.clear_kill)
+        self.action.start()
+        self.to_pose = PoseTwistStamped()
+        self.temp_pose = PoseTwistStamped()
+        self.killed = False
+
+
+    def set_kill(self):
+
         self.to_pose = PoseTwistStamped()
 
-    def new_goal(self, goal):
-        self.client.set_succeeded()
-        self.to_pose.header = goal.header
-        self.to_pose.posetwist = goal.posetwist
+        # TODO --> Verify that all values are at 0
 
+        #members = [attr for attr in dir(self.to_pose.posetwist.pose) if attr.startswith("position") or attr.startswith("orientation")]
+        #getattr(self.to_pose.posetwist.pose.position, "x") == 0
+        rospy.logwarn('Azi_Drive waypoint kill flag off -- All trajectories at 0: %s' % self.kill_listener.get_kills())
+        self.killed = True
+        
+    def clear_kill(self):
+        self.killed = False
+        rospy.logwarn('Azi_Drive waypoint kill flag off -- Waypoints enabled: %s' % self.kill_listener.get_kills())
+
+    def new_goal(self, goal):
+        self.temp_pose = PoseTwistStamped()
+        self.temp_pose.posetwist = goal.posetwist
+        self.temp_pose.header = goal.header
+        self.action.set_succeeded()
 
     def over_and_over(self):
         r = rospy.Rate(1)
-        self.trajectory_pub.publish(self.to_pose)
-        print self.to_pose
+        if self.killed == True:
+            rospy.logwarn('Azi_Drive waypoint kill flag on  -- Waypoints disabled: %s' % self.kill_listener.get_kills())
+        if self.killed == False:
+            self.to_pose = self.temp_pose
+            self.trajectory_pub.publish(self.to_pose)
+            print self.to_pose.posetwist.pose
+            self.to_pose = PoseTwistStamped()
         r.sleep()
 
-
-
 if __name__ == '__main__':
-    
-        # Initializes a rospy node so that the SimpleActionaction can
-        # publish and subscribe over ROS.
         rospy.init_node('azi_waypoint')
         action = send_action()
-        while(1):
+        while not rospy.is_shutdown():
             action.over_and_over()
 
             
