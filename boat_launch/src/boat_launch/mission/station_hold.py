ad16678a72337b45e96daec36cdc44d8ff05fb7d
Zach Goins <zach.a.goins@gmail.com>
2015-05-23 19:05:19 -0400

    Station_hold: convert to proper msg type

--- boat_launch/src/boat_launch/mission/station_hold.py
+++ boat_launch/src/boat_launch/mission/station_hold.py
@@ -3,58 +3,73 @@
 import roslib
 import rospy
 import actionlib
-from uf_common.msg import PoseTwistStamped, MoveToAction, PoseTwist, Acceleration
+from uf_common.msg import *
 from geometry_msgs.msg import Pose, Twist, Vector3, Point, Quaternion
+from actionlib_msgs.msg import GoalID
 from nav_msgs.msg import Odometry
-from std_msgs.msg import Header
+from std_msgs.msg import Header, Float64
 
-count = 0
 
-waypoint_point = rospy.Publisher('/trajectory', PoseTwistStamped, queue_size=1)
+def done():
+    rospy.loginfo("New Position Accepted")
+    os._exit(0)
 
 def odom_callback(current):
-    global count
-    waypoint_point.publish(PoseTwistStamped(
+    print 'Waiting for server...'
+    client = actionlib.SimpleActionClient('moveto', MoveToAction)
+    client.wait_for_server()
+    to_send = MoveToActionGoal(
             header = Header(
                 stamp=rospy.Time.now(),
                 frame_id="/base_link",
+            ),
+            goal_id = GoalID(
+                    stamp=rospy.Time.now(),
+                    id = "/goal"
+            ),
+            goal = MoveToGoal(
+                header = Header(
+                    stamp=rospy.Time.now(),
+                    frame_id="/base_link",
                 ),
-            posetwist = PoseTwist(
-                pose = Pose(
-                    position = Point(x = current.pose.pose.position.x,
-                                     y = current.pose.pose.position.y,
-                                     z = current.pose.pose.position.z),
+                posetwist = PoseTwist(
+                    pose = Pose(
+                        position = Point(x = current.pose.pose.position.x,
+                                         y = current.pose.pose.position.y,
+                                         z = current.pose.pose.position.z),
 
-                    orientation = Quaternion(x = current.pose.pose.orientation.x,
-                                             y = current.pose.pose.orientation.y,
-                                             z = current.pose.pose.orientation.z, 
-                                             w = current.pose.pose.orientation.w)
+                        orientation = Quaternion(x = current.pose.pose.orientation.x,
+                                                 y = current.pose.pose.orientation.y,
+                                                 z = current.pose.pose.orientation.z, 
+                                                 w = current.pose.pose.orientation.w)
+                    ),
+                    twist = Twist(
+                        linear = Vector3(x = current.twist.twist.linear.x,
+                                         y = current.twist.twist.linear.y,
+                                         z = current.twist.twist.linear.z),
+                    
+                        angular = Vector3(x = current.twist.twist.angular.x,
+                                          y = current.twist.twist.angular.y,
+                                          z = current.twist.twist.angular.z)
+                    ),
+                    acceleration = Acceleration(
+                        linear = Vector3(x=0,y=0,z=0),
+                        angular = Vector3(x=0,y=0,z=0)
+                    )
                 ),
-                twist = Twist(
-                    linear = Vector3(x = current.twist.twist.linear.x,
-                                     y = current.twist.twist.linear.y,
-                                     z = current.twist.twist.linear.z),
-                
-                    angular = Vector3(x = current.twist.twist.angular.x,
-                                      y = current.twist.twist.angular.y,
-                                      z = current.twist.twist.angular.z)
-                ),
-                acceleration = Acceleration(
-                    linear = Vector3(x=0,y=0,z=0),
-                    angular = Vector3(x=0,y=0,z=0)
-                )
-
-                )
+                speed = 0,
+                uncoordinated = False,
+                linear_tolerance = 0,
+                angular_tolerance = 0
             )
-                
         )
-    count = count + 1
+                
+    client.send_goal(to_send, done_cb = done)
 
 
 rospy.init_node('azi_waypoint_reset')
 rospy.Subscriber('/odom', Odometry, odom_callback)
-while count < 2:
-    None
-rospy.loginfo("New Position Accepted")
+rospy.spin()
+
 
             
