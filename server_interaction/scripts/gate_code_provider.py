59310b4ce0ece61c65a76e94f10288a4e1303c77
Gabriel Tapizquent <gaboth22@gmail.com>
2015-05-30 12:38:05 -0400

    Added JSON server

---
+++ server_interaction/scripts/gate_code_provider.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+import rospy
+import roslib
+roslib.load_manifest('server_interaction')
+import requests
+import json
+import sys
+import time
+from std_msgs.msg import String
+from server_interaction.msg import gate_code
+
+
+
+def StoreCourseCode(courseCode):
+
+	global course 
+
+	course = courseCode.data
+
+def SendRequest(serverUrl):
+	try:
+
+		mainUrl = serverUrl.data
+
+		sublinkMain = '/obstacleAvoidance/%s/UF/gateCode.json' %course
+
+		url = mainUrl +  sublinkMain
+
+		print(url)
+
+		r = requests.get(url)	
+				
+		if(r.status_code == 200):
+				
+			gatecode_all = r.json()['gateCode']
+
+			temp = gatecode_all.split(",")
+
+			entrance = temp[0].replace("(","")
+
+			exit = temp[1].replace(")","")
+
+			#ready message data to be pubslished
+
+			msg = gate_code()
+
+			msg.entrance = entrance
+
+			msg.exit= exit
+
+			gatecode_pub = rospy.Publisher('gate_code_data', gate_code, queue_size=10)
+			
+			rate = rospy.Rate(10)
+			
+			while not rospy.is_shutdown():
+
+				rospy.loginfo(msg)
+				
+				gatecode_pub.publish(msg)
+				
+				rate.sleep()
+
+		else:
+
+			raise rospy.ServiceException
+	
+	except (rospy.ServiceException, NameError) as e:
+		
+		pass		
+
+def main():
+
+	rospy.init_node('gate_code_provider')
+
+	rospy.Subscriber('course_code', String, StoreCourseCode)
+
+	rospy.Subscriber('main_server_url', String, SendRequest)
+
+	rospy.spin()
+
+if __name__ == '__main__':
+
+	try:
+
+		main()
+
+	except rospy.ROSInterruptException:
+		
+		pass	
---
+++ server_interaction/scripts/url_scanner.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import rospy
+import sys
+from std_msgs.msg import String
+
+## Scans information pertaining the server
+
+
+
+def ScanMainUrl():
+	
+	mainurl = raw_input("What is the MAIN server URL?: ")
+	
+	return mainurl
+
+def ScanCourseCode():
+
+	coursecode = raw_input("What COURSE are we attempting?: ")
+	
+	return coursecode
+
+def ScanCurrentChallenge():
+
+	currentchallenge = raw_input("what CHALLENGE are we attemting?: ")
+
+	return currentchallenge	
+
+
+def main():
+
+	mainurl = ScanMainUrl()
+
+	coursecode = ScanCourseCode()
+
+	currentchallenge = ScanCurrentChallenge()
+
+	mainurl_pub = rospy.Publisher('main_server_url', String, queue_size=10)
+	
+	coursecode_pub = rospy.Publisher('course_code', String, queue_size=10)
+
+	currentchallenge_pub = rospy.Publisher('current_challenge', String, queue_size=10)
+	
+	rospy.init_node('url_challenge_course_provider')
+	
+	rate = rospy.Rate(20)
+
+	while not rospy.is_shutdown():
+		
+		rospy.loginfo(mainurl)
+		
+		rospy.loginfo(coursecode)
+
+		rospy.loginfo(currentchallenge)
+		
+		mainurl_pub.publish(mainurl)
+		
+		coursecode_pub.publish(coursecode)
+
+		currentchallenge_pub.publish(currentchallenge)
+		
+		rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		main()
+	except rospy.ROSInterruptException:
+		pass	
