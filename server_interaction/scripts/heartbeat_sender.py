459a5aff0c6017e2e6720ef42c9c5f07607f2b5b
Gabriel Tapizquent <gaboth22@gmail.com>
2015-06-05 15:53:53 -0400

    fixed launch files

--- server_interaction/scripts/heartbeat_sender.py
+++ server_interaction/scripts/heartbeat_sender.py
@@ -12,7 +12,7 @@ from nav_msgs.msg import Odometry
 import time
 import math
 
-## This node just send the heartbeat to the server.
+## This node just sends the heartbeat to the server.
 
 def StoreMainServerUrl(serverUrl):
 
@@ -39,7 +39,7 @@ def GetCurrentTask(current_challenge):
 
 	currentChallenge = current_challenge.data
 
-
+#convert ecef to latitude and longitude
 def ecef2lla(ecef):
 
 	a = 6378137 # earths's radius in meters
@@ -144,11 +144,11 @@ def GetGpsData(gpsPos):
 
 		longitude = longitude[0:10]	
 
-		sublinkMain = '/heartbeat/%s/UF' %course
+		#sublinkMain = '/heartbeat/%s/UF' %course
 
 		#url = mainUrl +  sublinkMain
 
-		#d ump server to test that data is being posted
+		#dump server to test that data is being posted
 		url = 'https://posttestserver.com/post.php'
 
 		#payload to send to server
