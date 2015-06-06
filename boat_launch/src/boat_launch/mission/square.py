2344a9fce6f7bb4324ca2dd0a88e8e8c59a81d49
Kevin French <fnivek@gmail.com>
2015-06-06 16:15:19 -0400

    Fixed square mission

--- boat_launch/src/boat_launch/mission/square.py
+++ boat_launch/src/boat_launch/mission/square.py
@@ -5,7 +5,7 @@ from std_msgs.msg import Float64
 from txros import util
 import rospy
 import boat_scripting
-import gain_control
+#import gain_control
 
 @util.cancellableInlineCallbacks
 def main(nh):
@@ -13,8 +13,8 @@ def main(nh):
 
     for i in xrange(4):
         print 'Side', i
-        gain_control.less_gain()
-        yield boat.move.forward(-5).go()
-        gain_control.more_gain()
-        yield boat.move.turn_left_deg(45).go()
+        #gain_control.less_gain()
+        yield boat.move.forward(-10).go()
+        #gain_control.more_gain()
+        yield boat.move.turn_left_deg(90).go()
 
