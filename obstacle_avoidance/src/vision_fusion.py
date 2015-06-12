0faad24ab3a06a2afdaf5a2163ebc7cde149239c
Zach Goins <zach.a.goins@gmail.com>
2015-06-12 18:14:52 -0400

    lidar_fusion: Rosify lidar fusion

--- obstacle_avoidance/src/vision_fusion.py
+++ obstacle_avoidance/src/vision_fusion.py
@@ -3,9 +3,19 @@
 import numpy 
 import cv2
 import math
+from sensor_msgs.msg import PointCloud2, PointField
+import sensor_msgs.point_cloud2 as pc2
+import rospy
+from roslib import message
+
 
 desired_x = 960
 desired_y = 540
+
+x_compare_vect = numpy.array([1,0,0])
+y_compare_vect = numpy.array([0,1,0])
+z_compare_vect = numpy.array([0,0,1])
+
 
 '''
 
@@ -14,15 +24,32 @@ Vector notation: [x y z]
 '''
 
 class lidar_theta(object):
+
 	def __init__(self):
-		self.center_vector = numpy.array([0,0,1])
+		self.x_center_vector = x_compare_vect
+		self.y_center_vector = y_compare_vect
+		self.z_center_vector = z_compare_vect
+
 		self.focus_vector = numpy.array([1,1,1])
+
+		self.x = 0
+		self.y = 0
+		self.z = 0
+
 		self.angle_in_radians = 0
 		self.angle_in_degrees = 0
 
-	def find_angle(self):
-		numerator = numpy.dot(self.center_vector, self.focus_vector)
-		center_magnitude = self.find_magnitude(self.center_vector)
+		rospy.Subscriber("/lidar/raw_pc", PointCloud2, self.pointcloud_callback)
+
+	def solve_angles(self, obj_vect):
+		if obj_vect == 1: self.find_angle(self.x_center_vector)
+		if obj_vect == 2: self.find_angle(self.y_center_vector)
+		if obj_vect == 3: self.find_angle(self.z_center_vector)
+
+	def find_angle(self, obj_vect):
+
+		numerator = numpy.dot(obj_vect, self.focus_vector)
+		center_magnitude = self.find_magnitude(obj_vect)
 		focus_magnitude = self.find_magnitude(self.focus_vector)
 		cosine = numerator / (center_magnitude * focus_magnitude)
 
@@ -39,6 +66,33 @@ class lidar_theta(object):
 							  )
 
 		return magnitude
+
+	def pointcloud_callback(self, data) :
+
+	    data_out = pc2.read_points(data, field_names=("x", "y", "z"), skip_nans=False, uvs=[])
+	    int_data = next(data_out)
+	    self.x = int_data[0]
+	    self.y =  int_data[2]
+	    self.z = int_data[1]
+	    self.focus_vector = numpy.array([self.x, self.y, self.z])
+	    # y = 0
+	    # 
+	    #
+	    self.solve_angles(1)
+	    self.solve_angles(2)
+	    self.solve_angles(3)
+
+
+
+	def callback(self, data):
+		gen = pc2.read_points(data, skip_nans=True, field_names=("x", "y", "z"))
+		self.y =  int (data.height / 2)
+		self.x = int (data.width / 2)
+		self.y = read_depth (middle_x, height, data)
+		# do stuff with middle
+
+
+		
 
 class theta_return(object):
 	def __init__(self):
@@ -81,11 +135,20 @@ class theta_return(object):
 		print "Y theta in Radians: ", self.theta
 
 
+
 if __name__ == '__main__':
 
+	rospy.init_node("test")
 	lidar = lidar_theta()
-	lidar.find_angle()
+	lidar.solve_angles(1)
 
+	lidar.solve_angles(2)
+
+	lidar.solve_angles(3)
+
+	laser_projector = PointCloud2()
+
+	rospy.spin()
 	'''
 	 theta = theta_return()
 	 theta.solve_x_theta()
