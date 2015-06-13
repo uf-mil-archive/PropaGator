2b2a8a4e781733df630bf352903f15db04c82d9f
Lucas Bassett-Audain <lbassettaudain@ufl.edu>
2015-06-13 18:53:39 -0400

    MISSIONS: add start gate vision mission

---
+++ boat_launch/src/boat_launch/mission/start_gate_vision.py
@@ -0,0 +1,25 @@
+import numpy
+import math
+from txros import util
+import cv2
+import boat_scripting
+
+@util.cancellableInlineCallbacks
+def main(nh):
+    print "Q"
+    boat = yield boat_scripting.get_boat(nh)
+    case = True
+    while case == True:
+        print 'find gates'
+        #img = cv2.imread('Images/2014-4.jpg')
+        angle = yield boat.get_start_gate_vision()    
+        angle = angle
+        if abs(angle) < (numpy.pi/10):
+            print "forward"
+            yield boat.move.forward(14).go                        
+        elif angle < 0:
+            print "turn_left: ", angle
+            yield boat.move.turn_left(abs(angle))
+        elif angle > 0:
+            print "turn_right: ", angle
+            yield boat.move.turn_right(angle)
