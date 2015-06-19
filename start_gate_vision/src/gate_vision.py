385f9061326c02deee4e11544d4e61a2cb064516
Lucas Bassett-Audain <lbassettaudain@ufl.edu>
2015-06-19 13:04:00 -0400

    START_GATE_VISION: Add immage publisher

--- start_gate_vision/src/gate_vision.py
+++ start_gate_vision/src/gate_vision.py
@@ -18,7 +18,7 @@ class image_converter:
 
   def __init__(self):
     self.angle_pub = rospy.Publisher("start_gate_vision", Float64, queue_size=1)
-    #self.image_pub = rospy.Publisher("image_topic_2",Image)
+    self.image_pub = rospy.Publisher("image_topic_2",Image)
 
 
     cv2.namedWindow("Image window", 1)
@@ -29,12 +29,12 @@ class image_converter:
   
   def callback(self,data):
     try:
-      img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError, e:
       print e
-
+    cv_image2 = cv_image
+    img = cv_image
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)#HSV is good for color detection with lighting flux  
-
     #thresholding and building heat mask
     h1 = 45
     s1 = 163
@@ -48,6 +48,8 @@ class image_converter:
     temp = np.array(0)
     mask = cv2.inRange(img, lower_value, upper_value)
     bImg = cv2.bitwise_or(mask, temp)
+    img = cv2.rectangle(img,(0,0),(960,200),(0,0,0),-1)
+    #img = cv2.rectangle(img,(0,0),(50,480),(0,0,0),-1)
     #kernel = np.ones((150,150),np.uint8)
     #bImg = cv2.morphologyEx(bImg, cv2.MORPH_CLOSE, kernel)#messed something up was included in a tutorial not entirely required here
     ret, thresh = cv2.threshold(bImg, 127, 255, 0)
@@ -69,23 +71,24 @@ class image_converter:
     contours = contour_area_quick_sort(contours)
     moment0 = cv2.moments(contours[0])
     cx0 = int(moment0['m10']/moment0['m00'])
-    img = cv2.line(img,(cx0,0),(cx0,10000),(0,0,0),5)
+    #cv_image2 = cv2.line(cv_image2,(cx0,0),(cx0,480),(0,0,255),5)
     moment1 = cv2.moments(contours[1])
     cx1 = int(moment1['m10']/moment1['m00'])
-    img = cv2.line(img,(cx1,0),(cx1,10000),(0,0,0),5)
+    #cv_image2 = cv2.line(cv_image2,(cx1,0),(cx1,480),(0,255,0),5)
     #contours = cv2.findContours(thresh, 1, 2)#findimg contours really just masses of colors
     #img = cv2.drawContours(img, contours, 0, (0,0,0), 3)
     #img = cv2.drawContours(img, contours, 1, (0,0,0), 3)
     collums_midpoint = (cx0+cx1)/2
     midpoint_rela_centerline = collums_midpoint - 480
     angle = midpoint_rela_centerline*((np.pi*5/9)/960)#960 pixels over 5pi/9 radians view
-    '''
-    cv_image = cv2.line(img,(collums_midpoint,0),(collums_midpoint,10000),(0,255,0),5)
+    cv_image2 = cv2.line(cv_image2,(collums_midpoint,0),(collums_midpoint,480),(255,0,0),5)
+    print 'collums_midpoint', collums_midpoint
+    print 'biggest', cx0, 'second', cx1     
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
     except CvBridgeError, e:
       print e
-    '''
+    
 
     #cnt = contours[0]                        #selecting main contour    
     #x,y,w,h = cv2.boundingRect(cnt)
