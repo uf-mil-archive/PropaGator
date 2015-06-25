607fc742b9ede115fa352dde0bb668dfab86aa48
Lucas Bassett-Audain <lbassettaudain@ufl.edu>
2015-06-24 21:16:24 -0400

    START_GATE_VISION: Tune values at lake

--- boat_launch/src/boat_launch/mission/start_gate_vision.py
+++ boat_launch/src/boat_launch/mission/start_gate_vision.py
@@ -21,16 +21,20 @@ def main(nh, boat=None):
         #yield boat.move.as_MoveToGoal([3,0,0],angle).go()
 
         
-        if abs(angle) < (numpy.pi/8):
+        if abs(angle) < (numpy.pi/12):
             print "forward"
-            yield boat.move.forward(2).go()                       
+            yield boat.move.forward(6).go()
+            yield util.sleep(0.5)                       
             print "forward command sent"
         elif angle < 0:
             print "turn_left: ", angle/5
             yield boat.move.turn_left(abs(angle/5)).go()
+            yield util.sleep(0.5)
         elif angle > 0:
             print "turn_right: ", angle/5
             yield boat.move.turn_right(angle/5).go()
+            yield util.sleep(0.5)
+
         '''
         print 'left'
         yield boat.move.turn_left(abs(numpy.pi/3)).go()
