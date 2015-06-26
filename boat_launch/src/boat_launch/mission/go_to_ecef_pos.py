f3ab11165246d52648d0ffad91e6d42e547ede80
Kevin French <fnivek@gmail.com>
2015-06-26 01:02:40 -0400

    GO_ECEF_POS: Fix go_to_ecef_pos mission

--- boat_launch/src/boat_launch/mission/go_to_ecef_pos.py
+++ boat_launch/src/boat_launch/mission/go_to_ecef_pos.py
@@ -3,19 +3,23 @@
 from __future__ import division
 from txros import util
 import boat_scripting
-import station_hold
-from std_msgs.msg import Bool 
-import rospy
+from uf_common import orientation_helpers
+from rawgps_common import gps
+import numpy
 
 @util.cancellableInlineCallbacks
-def main(self, pos, speed=0, turn=True):
-    print 'go_to_ecef_pos', pos, speed, turn
+def main(nh, pos, speed=0, turn=True):
+    print 'go_to_ecef_pos: ', pos, "\n\tAt: ", speed, 'm/s\n\t', 'Turn first: ', turn
+
+    boat = yield boat_scripting.get_boat(nh)
+
     success = False
     try:
         first = True
+        last_enu_pos = numpy.zeros(3)
         while True:
-            odom_df = self._odom_sub.get_next_message()
-            abs_msg = yield self._absodom_sub.get_next_message()
+            odom_df = boat.get_gps_odom_rel()
+            abs_msg = yield boat.get_gps_odom()
             msg = yield odom_df
             
             error_ecef = pos - orientation_helpers.xyz_array(abs_msg.pose.pose.position)
@@ -24,21 +28,34 @@ def main(self, pos, speed=0, turn=True):
             print error_enu, '=>', numpy.linalg.norm(error_enu), '->', 1
             
             enu_pos = orientation_helpers.xyz_array(msg.pose.pose.position) + error_enu
-            enu_pos[2] = self.pose.position[2]
+            enu_pos[2] = boat.pose.position[2]
             
             if numpy.linalg.norm(error_enu) < 1:
-                yield self.move.set_position(enu_pos).go()
+                yield boat.move.set_position(enu_pos).go()
                 print 'go_to_ecef_pos', 'succeeded'
+                success = True
                 return
             
+            # Turn and send initial move incase enu_pos is within half a metter of [0,0,0]
             if first and turn:
-                yield self.move.look_at_without_pitching(enu_pos).go(speed=speed)
+                if turn:
+                    yield boat.move.look_at_without_pitching(enu_pos).go(speed=speed)
+                boat._moveto_action_client.send_goal(
+                    boat.pose.set_position(enu_pos).as_MoveToGoal(speed=speed)).forget()
                 first = False
             
-            self._moveto_action_client.send_goal(
-                self.pose.set_position(enu_pos).as_MoveToGoal(speed=speed)).forget()
+            # Check if the position drifted more than half a meter radius
+            #   if so send a new goal
+            if numpy.linalg.norm(last_enu_pos - enu_pos) > 0.5:
+                print 'Set new position'
+                boat._moveto_action_client.send_goal(
+                    boat.pose.set_position(enu_pos).as_MoveToGoal(speed=speed)).forget()
+
+            last_enu_pos = enu_pos
+
         success = True
     finally:
         if not success:
-            yield self.move.set_position(self.odom.position).go() # stop moving
-            yield self.move.backward(3).go()
+            print 'go_to_ecef_pos failed to attain desired position, holding current position'
+            yield boat.move.set_position(boat.odom.position).go() # stop moving
+            #yield boat.move.backward(3).go() # I have no idea why this needs to happen
--- boat_launch/src/boat_launch/mission/test_boat_class.py
+++ boat_launch/src/boat_launch/mission/test_boat_class.py
@@ -1,11 +1,19 @@
 from txros import util
 import boat_scripting
+from boat_launch.mission import go_to_ecef_pos
+from rawgps_common import gps
+import math
+
+# TODO: Move this to some common place to be used across files
+def ll(lat, lon):
+    return gps.ecef_from_latlongheight(math.radians(lat), math.radians(lon), 0)
 
 @util.cancellableInlineCallbacks
-def main(nh):
+def main(nh, location='lake_alice'):
 	boat = yield boat_scripting.get_boat(nh)
 
 	#boat.pan_lidar(freq = 2, min_angle = 2.9, max_angle=3.14)
+	yield go_to_ecef_pos.main(nh, ll(36.80190, -76.19136))
 
 	while True:
 		for i in xrange(0,3):
