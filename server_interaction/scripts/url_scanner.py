7a9b081ed8e7e5fe983ad72679396a5e21af4c58
Gabriel Tapizquent <gaboth22@gmail.com>
2015-06-26 14:27:12 -0400

    SERVER_INTERACTION: Update url scanner

--- server_interaction/scripts/url_scanner.py
+++ server_interaction/scripts/url_scanner.py
@@ -6,28 +6,24 @@ from std_msgs.msg import String
 ## Scans information pertaining the server
 def ScanMainUrl():
 	
-	mainurl = raw_input("What is the MAIN server URL?: ")	
-	return mainurl
+	mainurl = raw_input("\033[1;34mWhat is the main SERVER URL?: \033[0m")
+	if "http://" not in mainurl:
+		mainurl = "http://"+mainurl
+		return mainurl
+	else:		
+		return mainurl
 
 def ScanCourseCode():
 
-	coursecode = raw_input("What COURSE are we attempting?: ")
+	coursecode = raw_input("\033[1;34mWhat COURSE are we attempting?: \033[0m")
 	return coursecode
-
-def ScanCurrentChallenge():
-
-	currentchallenge = raw_input("what CHALLENGE are we attemting?: ")
-	return currentchallenge	
-
 
 def main():
 
 	mainurl = ScanMainUrl()
 	coursecode = ScanCourseCode()
-	currentchallenge = ScanCurrentChallenge()
 	mainurl_pub = rospy.Publisher('main_server_url', String, queue_size=10)
 	coursecode_pub = rospy.Publisher('course_code', String, queue_size=10)
-	currentchallenge_pub = rospy.Publisher('current_challenge', String, queue_size=10)
 	rospy.init_node('url_challenge_course_provider')
 	rate = rospy.Rate(1)
 
@@ -35,10 +31,8 @@ def main():
 		
 		rospy.loginfo(mainurl)	
 		rospy.loginfo(coursecode)
-		rospy.loginfo(currentchallenge)
 		mainurl_pub.publish(mainurl)
 		coursecode_pub.publish(coursecode)
-		currentchallenge_pub.publish(currentchallenge)
 		rate.sleep()
 
 if __name__ == '__main__':
