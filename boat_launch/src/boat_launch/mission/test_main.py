23337f415e128a7ded615f86acdd58fe10d236ee
Zach Goins <zach.a.goins@gmail.com>
2015-06-27 10:52:22 -0400

    MISSION: Test mission ready for lake day

--- boat_launch/src/boat_launch/mission/test_main.py
+++ boat_launch/src/boat_launch/mission/test_main.py
@@ -14,23 +14,28 @@ SPEED = 1
 
 ONE_MINUTE = 60
 
-TOTAL TIME = 20 * ONE_MINUTE
+TOTAL_TIME = 20 * ONE_MINUTE
 CIRCLE_TIME = ONE_MINUTE
 TRIANGLE_TIME = ONE_MINUTE
 CROSS_TIME = ONE_MINUTE
 ECEF_TIME = ONE_MINUTE
 START_GATE_TIME = ONE_MINUTE
 
+NORTH = np.pi/2
+EAST = 0
+SOUTH = (3*np.pi)/2
+WEST = np.pi
+
 # LAKE WAYPOINTS
 
-WAYPOINT_A = (29.641090, -82.361292)
-WAYPOINT_B = (29.641260, -82.361197)
-WAYPOINT_C = (29.641394, -82.361293)
-WAYPOINT_D = (29.641651, -82.361444)
-WAYPOINT_E = (29.641245, -82.361014)
-WAYPOINT_F = (29.641360, -82.361104)
-WAYPOINT_G = (29.641104, -82.361470)
-WAYPOINT_H = (29.641274, -82.361403)
+WAYPOINT_A = ll(29.641090, -82.361292)
+WAYPOINT_B = ll(29.641260, -82.361197)
+WAYPOINT_C = ll(29.641394, -82.361293)
+WAYPOINT_D = ll(29.641651, -82.361444)
+WAYPOINT_E = ll(29.641245, -82.361014)
+WAYPOINT_F = ll(29.641360, -82.361104)
+WAYPOINT_G = ll(29.641104, -82.361470)
+WAYPOINT_H = ll(29.641274, -82.361403)
 
 @util.cancellableInlineCallbacks
 def do_dock(nh, shape):
@@ -63,11 +68,28 @@ def do_dock(nh, shape):
 @util.cancellableInlineCallbacks
 def main(nh):
 
+	assert TOTAL_TIME > CIRCLE_TIME + CROSS_TIME + TRIANGLE_TIME + (ECEF_TIME * 4) + START_GATE_TIME
+
+	'''
+
+	Test Main Mission:
+
+	1. Go to point A and turn North, then begin speed gate challenge - One minute timeout
+	2. Do speedgates, then go to point F and turn South - One minute timeout
+	3. Go to position E to begin docking challenges - One minute each
+	4. When done with docking move back to point A and turn north
+
+	SPEED ONLY SET TO ONE TO START
+
+	'''
+
 	print "Starting main mission"
 
 	print "Moving to first position: ", WAYPOINT_A
+
 	try:
 		yield util.wrap_timeout(go_to_ecef_pos.main(nh, WAYPOINT_A, SPEED, False) ECEF_TIME)
+		yield boat.move.heading(NORTH).go()
 		print "Arrived at first position"
 	except Exception:
 		print "Could not make it to first position in " + ECEF_TIME + " seconds"
@@ -75,7 +97,6 @@ def main(nh):
 
 	print "Starting speed gates now"
 	try:
-
 		start_gate = yield util.wrap_timeout(start_gate_vision.main(nh), ONE_MINUTE)
 		print "Startgates completed succesfully!"
 	except Exception:
@@ -86,12 +107,14 @@ def main(nh):
 
 	try:
 		yield util.wrap_timeout(go_to_ecef_pos.main(nh, WAYPOINT_F, SPEED, False) ONE_MINUTE)
+		yield boat.move.heading(SOUTH).go()
 		print "Moved to " + WAYPOINT_F + ", now moving to " + WAYPOINT_E + " to begin docking"
 	except Exception:
 		print "Could not make it to second position in " + ONE_MINUTE + " seconds"
 
 	try:
 		yield util.wrap_timeout(go_to_ecef_pos.main(nh, WAYPOINT_E, SPEED, False) ONE_MINUTE)
+		yield boat.move.heading(EAST).go()
 		print "Moved to " + WAYPOINT_E + ", starting docking challenge"
 	except Exception:
 		print "Could not make it to third position in " + ONE_MINUTE + " seconds"
@@ -115,11 +138,13 @@ def main(nh):
 		#traceback.print_exc()
 
 
+
 	print "Docking complete"
 	print "Moving back to startate begining position", WAYPOINT_A
 
 	try:
 		yield util.wrap_timeout(go_to_ecef_pos.main(nh, WAYPOINT_A, SPEED, False) ONE_MINUTE)
+		yield boat.move.heading(NORTH).go()
 		print "Arrived at ", WAYPOINT_A
 	except Exception:
 		print "Could not make it to third position in " + ONE_MINUTE + " seconds"
