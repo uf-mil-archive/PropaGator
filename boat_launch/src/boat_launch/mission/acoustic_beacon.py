d6610685d8551c596f77ba4847ab7b68c7f339bc
Kevin French <fnivek@gmail.com>
2015-07-02 15:58:49 -0400

    HYDROPHONE: Update acoustic_beacon mission

--- boat_launch/src/boat_launch/mission/acoustic_beacon.py
+++ boat_launch/src/boat_launch/mission/acoustic_beacon.py
@@ -1,63 +1,50 @@
-from __future__ import division
+import math 
+import traceback 
+from txros import util 
+import random 
 
-import math
-import traceback
-from txros import util
-import random
-
-# we will move the boat based on the info in the processed messages
 from hydrophones.msg import ProcessedPing
-# we will also subscribe to a simple message that tells us the freq to look fo
-from std_msgs.msg import Float64
-# we will publish the pingers lat long with a nav_msgs/Odometry message like the /absodom topic. the lat long is under pose.pose.position
-from geometry_msgs.msg import Point
 
 import boat_scripting
-
-#SPP the gps methods is exported here only fur testing currently
 from rawgps_common import gps
+from boat_launch.mission import hydrophone_align
 
-#NOTE: run missions does ros init node for us
-    
-
-@util.cancellableInlineCallbacks
-def _send_result(ci, course, lat, lon):
-    colors = set(['yellow', 'blue', 'black', 'green', 'red'])
-    while colors:
-        color = random.choice(list(colors))
-        try:
-            res = yield ci.send_pinger_answer(course, color, lat, lon)
-            print res
-            if res: return
-            if not res: colors.remove(color)
-        except:
-            traceback.print_exc()
-        yield util.sleep(5)
+course_alice = [29.641341, -82.361133]
 
 @util.cancellableInlineCallbacks
-def main(nh, ci, course, freq):
-    freq=27440
+def _send_result(course, lat, lon):
+    # Send over the results and the color of the buoy through the JSON server
+    # I will populate this field come lakeday 
+    pass
+
+@util.cancellableInlineCallbacks 
+def main(nh, course=course_alice, min_freq=27E3, max_freq=40E3):
+    # nh = Node Handler from txros
+    # course = Set of GPS points 
+    # freq = 27440 Remnant from previous mission file. I don't really know why this is here.
     boat = yield boat_scripting.get_boat(nh)
-    #float_df = boat.float()
-    #yield boat.retract_hydrophone() # why was this here? it shouldn't need this, the hydrophones should be up on start
-    
-    print "Starting ping mission"
-    print "Deploying Hydrophone"
-    yield boat.deploy_hydrophone()
-    print "Hydrophone Deployed"
+
     try:
-        yield util.wrap_timeout(boat.hydrophone_align(freq), 60*2)
-    except Exception:
-        traceback.print_exc()
-    print "Finished ping mission"
-    # Note: /absodom is Earth-Centered,Earth-Fixed (ECEF), so This means that ECEF rotates with the earth and a point fixed on the surface of the earth do not change.
-    # See: http://en.wikipedia.org/wiki/ECEF
-    msg = yield boat.get_gps_odom()
-    temp = gps.latlongheight_from_ecef([msg.pose.pose.position.x,msg.pose.pose.position.y,msg.pose.pose.position.z])
-    print "latitude: ", temp[0]," longitude: ", temp[1]
-    _send_result(ci, course, math.degrees(temp[0]), math.degrees(temp[1])) # async XXX
-    #float_df.cancel()
-    print "Retracting Hydrophone"
-    yield boat.retract_hydrophone()
-    print "Hydrophone Retracted"
-    print "Done"
+        print "Beginning ping mission" 
+        print "Deploying hydrophone..."
+        yield boat.deploy_hydrophone()
+        print "Hydrophone has been deployed"
+        
+        try:
+            yield util.wrap_timeout(hydrophone_align.main(nh, min_freq, max_freq), 60*2)
+        except Exception:
+            traceback.print_exc()
+        print "Ping mission has finished"
+
+        #temp = gps.latlongheight_from_ecef(boat.odom.position)
+        #print "latitude: ", temp[0], " longitude: ", temp[1]
+
+        # In the main mission state machine remember to rerun the retract hydrophone mission 
+        # in case this mission timesout.
+        print "Retracting hydrophone..."
+        yield boat.retract_hydrophone()
+        print "Hydrophone retracted"
+        print "Done"
+
+    finally:
+        boat.default_state()
